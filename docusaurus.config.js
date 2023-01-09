d9759b8d5f83762ec9fc523ff11e8d480ae38c7a
Ellie Huxtable <ellie@posthog.com>
2023-01-09 21:27:26 +0000

    Adjust title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Notes',
+  title: 'Ellie's Notes',
   tagline: 'Some scribblings from me',
   url: 'https://notes.ellie.wtf',
   baseUrl: '/',
